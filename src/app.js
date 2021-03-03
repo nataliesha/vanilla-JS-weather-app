9ed696bfb45fc7db9576aae0a57fa22325f15488
nataliesha <74931057+nataliesha@users.noreply.github.com>
2021-03-03 15:27:15 +0200

    Made Fahrenheit link work

--- src/app.js
+++ src/app.js
@@ -23,7 +23,9 @@ let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
 let iconElement = document.querySelector("#icon");
 
-temperatureElement.innerHTML = Math.round(response.data.main.temp);
+celsiusTemperature = response.data.main.temp;
+
+temperatureElement.innerHTML = Math.round(celsiusTemperature);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
@@ -46,7 +48,22 @@ function handleSubmit(event) {
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
   console.log(cityInputElement);
+  
 }
+
+function convertToFfahrenheit(event) {
+    event.preventDefault();
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+    
+
+}
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFfahrenheit);
