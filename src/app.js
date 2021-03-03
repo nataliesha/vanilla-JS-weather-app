a62a1aec5e080a1e4bf6018c580f068c094a19df
nataliesha <74931057+nataliesha@users.noreply.github.com>
2021-03-03 10:31:39 +0200

    Set icon

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
 
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
@@ -28,11 +29,12 @@ descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
 }
 
 let apiKey = "9c8bafe634beca68b8859c59c1ca6768";
-let city = "Zoran"
+let city = "Paris"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 
