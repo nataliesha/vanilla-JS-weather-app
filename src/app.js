ff230508fec53282343370e0ba3093f660a34c34
nataliesha <74931057+nataliesha@users.noreply.github.com>
2021-03-03 08:57:20 +0200

    Showing real data
    Humidity
    Wind:

---
+++ src/app.js
@@ -0,0 +1,23 @@
+
+function displaytemperature(response) {
+console.log(response.data);
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
+
+
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
+
+}
+
+let apiKey = "9c8bafe634beca68b8859c59c1ca6768";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+
+
+axios.get(apiUrl).then(displaytemperature);
